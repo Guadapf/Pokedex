d292b70481db2aa46711fdb52920152846fdfac1
Fernandez Guadalupe <guadapfernandez@gmail.com>
2023-09-20 15:00:50 -0300

    login funciona ok

--- validar.php
+++ validar.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-$_SESSION ["error"]= "Nombre y/o contraseña incorrectos";
 $_SESSION ["admin"] = false;
 
 $conexion = mysqli_connect("localhost", "root", "", "pokedex");
@@ -13,20 +12,34 @@ if(mysqli_connect_errno()){
 if(isset($_POST["nombre"]) && isset($_POST["password"])){
     $nombre = $_POST["nombre"];
     $password = $_POST["password"];
-    $_SESSION["nombre"] = $nombre;
+
 
     $query = "SELECT r.descripcion FROM usuario u JOIN rol r ON u.id_rol = r.id_rol WHERE u.nombre = '$nombre' AND u.password = '$password';";
     $resultado = mysqli_query($conexion, "$query");
 
-    if($resultado !== false){
+    if ($resultado !== false) {
         $rol = mysqli_fetch_assoc($resultado);
-        if($rol["descripcion"] == "admin") {
-            $_SESSION["admin"] = true;
+
+        switch ($rol["descripcion"]) {
+            case "admin":
+                $_SESSION["admin"] = true;
+                $_SESSION["nombre"] = $nombre;
+                break;
+            case "usuario":
+                $_SESSION["nombre"] = $nombre;
+                break;
+            default:
+                $_SESSION["error"] = "Nombre y/o contraseña incorrectos";
+                header("Location: vista-login.php");
+                exit();
         }
+
     } else {
+        $_SESSION["error"] = "Nombre y/o contraseña incorrectos";
         header("Location: vista-login.php");
         exit();
     }
+
     header("Location: index.php");
     exit();
 }
